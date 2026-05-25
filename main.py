1a82e1798be71b26bb09b26c74f2e836aba44fae
Kla-hub-2023 <kla.kaewwonghew@gmail.com>
2026-05-25 21:09:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ menu_options = []
 if user_role == "admin": menu_options = ["🏠 Dashboard", "➕ Booker", "🖥️ Dispatcher", "𚖖 งานของฉัน (Driver)", "✈️ Airport Staff"]
 elif user_role == "booker": menu_options = ["➕ Booker"]
 elif user_role == "dispatcher": menu_options = ["🖥️ Dispatcher"]
-elif user_role == "driver": menu_options = ["𚖖 งานของฉัน (Driver)"]
+elif user_role == "driver": menu_options = ["🚖 งานของฉัน (Driver)"]
 elif user_role == "airportstaff": menu_options = ["✈️ Airport Staff"]
 else: menu_options = ["📝 ลงทะเบียนพนักงานใหม่"]
 
